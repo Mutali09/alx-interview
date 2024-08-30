68d5fdbc975b4dea792086611f0bdaf53b15516b
Mutali09 <nelsonsimiyu9@gmail.com>
2024-08-30 16:36:27 +0300

    edits

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,17 @@
-import requests
-import sys
+#!/usr/bin/node
 
-def get_movie_characters(movie_id):
-    # Base URL of the Star Wars API
-    base_url = "https://swapi.dev/api/films/"
-    
-    # Get the movie data
-    response = requests.get(base_url + str(movie_id) + '/')
-    if response.status_code != 200:
-        print("Error: Unable to fetch data for the movie ID provided.")
-        return
-    
-    movie_data = response.json()
-    character_urls = movie_data['characters']
-    
-    # Fetch and print each character name
-    for url in character_urls:
-        character_response = requests.get(url)
-        if character_response.status_code == 200:
-            character_data = character_response.json()
-            print(character_data['name'])
-        else:
-            print("Error: Unable to fetch character data from", url)
+const request = require('request');
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-starwars_characters.js <Movie ID>")
-        sys.exit(1)
-    
-    movie_id = sys.argv[1]
-    get_movie_characters(movie_id)
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
