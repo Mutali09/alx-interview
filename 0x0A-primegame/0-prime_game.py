bcb0b9d027070b2c977f284232e7aa817e387e10
Mutali09 <nelsonsimiyu9@gmail.com>
2024-09-28 23:20:11 -0700

    changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,40 +3,47 @@
 
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
+    """Determine the winner of each round
+    Args:
+        x: number of rounds
+        nums: list of 'n' for each round
+    Returns:
+        The name of the player with the most wins or None if it's a draw
     """
-    if x <= 0 or nums is None:
-        return None
-    if x != len(nums):
+    if x <= 0 or nums is None or not nums:
         return None
 
     ben = 0
     maria = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
+    max_num = max(nums)  # Find the largest number to optimize the sieve
+    primes = sieve_of_eratosthenes(max_num)
 
-    for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
+    for n in nums:
+        primes_count = sum(primes[:n + 1])
+
+        # If the count of primes is even, Ben wins, otherwise Maria wins
+        if primes_count % 2 == 0:
             ben += 1
         else:
             maria += 1
+
     if ben > maria:
         return "Ben"
-    if maria > ben:
+    elif maria > ben:
         return "Maria"
-    return None
+    else:
+        return None
 
 
-def rm_multiples(ls, x):
-    """removes multiple
-    of primes
-    """
-    for i in range(2, len(ls)):
-        try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
-            break
+def sieve_of_eratosthenes(n):
+    """Sieve of Eratosthenes to find all primes up to n"""
+    sieve = [True] * (n + 1)
+    sieve[0], sieve[1] = False, False  # 0 and 1 are not primes
+
+    for start in range(2, int(n ** 0.5) + 1):
+        if sieve[start]:
+            for i in range(start * start, n + 1, start):
+                sieve[i] = False
+
+    return sieve
